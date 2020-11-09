93c78f7270528a87e06aacb9466f24038e7f6429
Tom <tjpalmer@pop-os.localdomain>
2020-11-09 07:18:28 -0700

    Finish for video

--- demo.cr
+++ demo.cr
@@ -1,7 +1,8 @@
+# Demo based on examples at crystal-lang.org
 # A very basic HTTP server
 require "http/server"
 
-def build_message(path)
+def make_message(path)
   path = path.to_s
   message = if path == "/"
     "world"
@@ -12,15 +13,17 @@ def build_message(path)
   message.upcase
 end
 
+port = 2020
+
 server = HTTP::Server.new do |context|
   context.response.content_type = "text/plain"
   # Output based on path.
-  message = build_message(context.request.path)
+  message = make_message(context.request.path)
   context.response.print "Hello #{message}!\n"
-  # # Hardcoded output.
-  # message2 = build_message(["from", 2020])
-  # context.response.print "Hello #{message2}!\n"
+  # Hardcoded output.
+  message2 = make_message(["from", port])
+  context.response.print "Hello #{message2}!\n"
 end
 
-puts "Listening on http://127.0.0.1:8080"
-server.listen(8080)
+puts "Listening on http://127.0.0.1:#{port}"
+server.listen(port)
